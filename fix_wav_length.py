37092556568c73e3eed9b7d5f28cffbd8ce2d361
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2011-09-17 13:22:15 -0400

    Clean up

--- fix_wav_length.py
+++ fix_wav_length.py
@@ -1,15 +1,4 @@
-import sys
 import struct
-from hachoir_parser import createParser
-
-def fix_length(filename):
-    if not isinstance(filename, unicode):
-        filename = filename.decode(sys.getfilesystemencoding())
-    root = createParser(filename)
-    fileSize = root.stream.size >> 3 # it's in bits!
-    root['/filesize'].value = fileSize - 8
-    root['/audio_data/size'].value = fileSize - (root['/audio_data'].address >> 3) - 8
-    return root
 
 def readAt(f, at, bytes):
     f.seek(at)
@@ -32,18 +21,16 @@ def readChunk(f, pos):
     chunkSize = struct.unpack('<I', readAt(f, pos+4, 4))[0]
     return chunkId, chunkSize
 
-def fix2(filename):
+def fix_wav_length(filename):
     f = open(filename, 'rb+')
     f.seek(0, 2) # seek to end
     fileSize = f.tell()
-    print fileSize
     f.seek(0)
     expect(f, 0, 'RIFF')
     pos = 8
     while True:
         chunkId, chunkSize = readChunk(f, pos)
         effSize = (chunkSize + 3) & (~3) # round up to 4
-        print pos, chunkId
         if chunkId == 'WAVE':
             pos += 4
             continue
@@ -56,6 +43,7 @@ def fix2(filename):
 if __name__ == '__main__':
     import sys
     for filename in sys.argv[1:]:
-        print filename
-        fix2(filename)
-        
+        try:
+            fix_wav_length(filename)
+        except RuntimeError, e:
+            print >>sys.stderr, "Error processing %r: %s" % (filename, e.message)
